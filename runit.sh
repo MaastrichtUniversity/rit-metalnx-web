89a63a61f909e2d804cd5a14a4c4c0410420d47e
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2021-10-05 16:25:45 +0200

    Provision the Metalnx secrets from docker-compose.yml [DHS-1044, DHS-1886]

--- runit.sh
+++ runit.sh
@@ -16,12 +16,25 @@ echo "Deploying war file..."
 mkdir $CATALINA_HOME/webapps/metalnx && \
   cd $CATALINA_HOME/webapps/metalnx && \
   jar -xf $CATALINA_HOME/webapps/metalnx.war && \
-#  ls -l $CATALINA_HOME/webapps/metalnx && \
   rm $CATALINA_HOME/webapps/metalnx.war && \
   cd $CATALINA_HOME/
 
 echo "Reconfiguring log level..."
 cp /tmp/log4j.properties /usr/local/tomcat/webapps/metalnx/WEB-INF/classes/log4j.properties
+
+echo "Reconfiguring Metalnx properties file..."
+env_file=/etc/irods-ext/metalnx.properties
+cp /tmp/metalnx.properties $env_file
+sed -ir "s|irods.host=.*$|irods.host=$IRODS_HOST|" $env_file
+sed -ir "s|irods.port=.*$|irods.port=$IRODS_PORT|" $env_file
+sed -ir "s|irods.zoneName=.*$|irods.zoneName=$IRODS_ZONE|" $env_file
+sed -ir "s|irods.admin.user=.*$|irods.admin.user=$IRODS_USER|" $env_file
+sed -ir "s|irods.admin.password=.*$|irods.admin.password=$IRODS_PASS|" $env_file
+sed -ir "s|jobs.irods.username=.*$|jobs.irods.username=$IRODS_USER|" $env_file
+sed -ir "s|jobs.irods.password=.*$|jobs.irods.password=$IRODS_PASS|" $env_file
+sed -ir "s|db.url=.*$|db.url=$METALNX_DB_URL|" $env_file
+sed -ir "s|db.username=.*$|db.username=$METALNX_DB_USER|" $env_file
+sed -ir "s|db.password=.*$|db.password=$METALNX_DB_PASS|" $env_file
 
 # Start tomcat
 echo "Starting Metalnx"
